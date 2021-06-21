1871b04320b26ab28b015ed7bb51d7a3b627b14d
Cothico <thiago.walli@gmail.com>
2021-06-21 04:42:27 -0300

    Atributos do firebase no jogo

--- Techin07/Assets/Senior/Assets/Scripts/UpdateXPCustom.cs
+++ Techin07/Assets/Senior/Assets/Scripts/UpdateXPCustom.cs
@@ -7,6 +7,12 @@ public class UpdateXPCustom : MonoBehaviour
 {
     XPManager xPManager;
     PlayerXP playerXP;
+    Moviment moviment;
+    StaminaBar staminaBar;
+    ManaC manaC;
+    Check check;
+    public bool isGame;
+    public GameObject loading;
     User user = new User();
     // Start is called before the first frame update
 
@@ -17,18 +23,36 @@ public class UpdateXPCustom : MonoBehaviour
     }
     void Start()
     {
-        StartCoroutine(UpdateLevels());
+        if(!isGame)
+        {
+            loading.SetActive(true);
+            Debug.Log("é falso mesmo");
+            StartCoroutine(UpdateLevels());
+        }
+        else{
+            moviment = FindObjectOfType<Moviment>();
+            staminaBar = FindObjectOfType<StaminaBar>();
+            manaC = FindObjectOfType<ManaC>();
+            check = FindObjectOfType<Check>();
+            moviment.atr_Habilidade = PlayerXP.levelHab; 
+            staminaBar.atributoForca = PlayerXP.levelFor;
+            manaC.inteligencia = PlayerXP.levelInt;
+            check.carisma = PlayerXP.levelCar;
+        }
+        
     }
 
     IEnumerator UpdateLevels()
     {
-        yield return new WaitForSeconds(0.15f);
+        yield return new WaitForSeconds(2f);
+        loading.SetActive(false);
         PlayerXP.totalLevel = PlayerXP.levelFor + PlayerXP.levelHab + PlayerXP.levelInt + PlayerXP.levelCar;
         playerXP.ptot.text = PlayerXP.totalLevel.ToString();
         playerXP.pfor.text = PlayerXP.levelFor.ToString();
         playerXP.pint.text = PlayerXP.levelInt.ToString(); 
         playerXP.pcar.text = PlayerXP.levelCar.ToString(); 
         playerXP.phab.text = PlayerXP.levelHab.ToString(); 
+        Debug.Log(user.userForLevel);
     }
 
     public void PlayGame01()
